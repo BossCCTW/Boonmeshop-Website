abf1ae605835954b888bbfdb02a00a0b0bd6ed08
BossCCTW <chsi.12j@gmail.com>
2018-03-20 01:58:38 +0700

    new update 20/3/2018

--- scripts/event_index.js
+++ scripts/event_index.js
@@ -1,228 +1,265 @@
 "use strict";
 
-let Navbar = (function(){
-    //Navbar DOM
-    let $navbar = $('.navbar').find('.navbar-container');
-    let $link = $navbar.find('.navbar-link');
-    let $webName = $navbar.find('.navbar-webname');
+let Navbar = (function () {
+  //Navbar DOM
+  let $navbar = $('.navbar').find('.navbar-container');
+  let $link = $navbar.find('.navbar-link');
+  let $webName = $navbar.find('.navbar-webname');
 
-    //register DOM
-    let $linkRegister = $link.find('.navbar-link_register');
-    let $modalRegisterContainer = $linkRegister.find('.modal-login');
-    let $modalRegisterDialog = $modalRegisterContainer.find('.modal-login_content');
-    let $closeBtnModalLogin = $modalRegisterDialog.find('.modal-btn-close');
+  //register DOM
+  let $linkRegister = $link.find('.navbar-link_register');
+  let $modalRegisterContainer = $linkRegister.find('.modal-login');
+  let $modalRegisterDialog = $modalRegisterContainer.find('.modal-login_content');
+  let $closeBtnModalLogin = $modalRegisterDialog.find('.modal-btn-close');
 
-    //download DOM
-    let $linkDownload = $link.find('li.navbar-link_download');
-    let $modalDownloadContainer = $linkDownload.find('.modal-download');
-    let $modalDownloadDialog = $modalDownloadContainer.find('.modal-download_content');
-    let $closeBtnModalDownload = $modalDownloadContainer.find('.modal-btn-close'); 
+  //download DOM
+  let $linkDownload = $link.find('li.navbar-link_download');
+  let $modalDownloadContainer = $linkDownload.find('.modal-download');
+  let $modalDownloadDialog = $modalDownloadContainer.find('.modal-download_content');
+  let $closeBtnModalDownload = $modalDownloadContainer.find('.modal-btn-close');
 
-    //Button Hamburger
-    let $btnHamburger = $navbar.find('.navbar-hamburger');
+  //Button Hamburger
+  let $btnHamburger = $navbar.find('.navbar-hamburger');
 
-    //Bind event Webname
-    $webName.on('click',() => window.location = '/');
-    //Bind event register
-    $linkRegister.on('click',{modalName:$modalRegisterContainer},showModal);
-    $closeBtnModalLogin.on('click',{modalName:$modalRegisterContainer},closeModal);
-    $modalRegisterContainer.on('click',{modalName:$modalRegisterContainer},closeModalOutside);
-    $modalRegisterDialog.on('click',stopCallParent);
-    //Bind event Donwload
-    $linkDownload.on('click',{modalName:$modalDownloadContainer},showModal);
-    $closeBtnModalDownload.on('click',{modalName:$modalDownloadContainer},closeModal);
-    $modalDownloadContainer.on('click',{modalName:$modalDownloadContainer},closeModalOutside);
-    $modalDownloadDialog.on('click',stopCallParent);
+  //Bind event Webname
+  $webName.on('click', () => window.location = '/');
+  //Bind event register
+  $linkRegister.on('click', {
+    modalName: $modalRegisterContainer
+  }, showModal);
+  $closeBtnModalLogin.on('click', {
+    modalName: $modalRegisterContainer
+  }, closeModal);
+  $modalRegisterContainer.on('click', {
+    modalName: $modalRegisterContainer
+  }, closeModalOutside);
+  $modalRegisterDialog.on('click', stopCallParent);
+  //Bind event Donwload
+  $linkDownload.on('click', {
+    modalName: $modalDownloadContainer
+  }, showModal);
+  $closeBtnModalDownload.on('click', {
+    modalName: $modalDownloadContainer
+  }, closeModal);
+  $modalDownloadContainer.on('click', {
+    modalName: $modalDownloadContainer
+  }, closeModalOutside);
+  $modalDownloadDialog.on('click', stopCallParent);
 
-    //Bind event Hamburger
-    $btnHamburger.on('click',toggleShowLink);
+  //Bind event Hamburger
+  $btnHamburger.on('click', toggleShowLink);
 
-   function showModal(event){
-      event.data.modalName.css('display','block');
-      console.log('Show modal : '+event.data.modalName[0].className);
-   }
-   function closeModal(event){ 
-       stopCallParent(event);
-       event.data.modalName.css('display','none');  
-       //โดยปกติแล้วโปรแกรมจะมองว่าการคลิกปุ่มที่อยู่ใน child เป็นการคลิก parentด้วย ถึงทำให้ handler ของ parent ทำงานอีกครั้งหลังจาก handler ของ child จบลง
-       //การใช้ stopPropagation เพื่อไม่ให้การ click มีผลต่อ parent ด้วย
-      
-       //Propagation หมายถึงการเผยแผ่
-       console.log('close by button-close : '+event.data.modalName[0].className);
-       
-   }   
-   function closeModalOutside(event){
-    stopCallParent(event);
-    event.data.modalName.css('display','none');
-    console.log('close by outside : '+event.data.modalName[0].className);
+  function showModal(event) {
+    event.data.modalName.css('display', 'block');
+    console.log('Show modal : ' + event.data.modalName[0].className);
   }
-  function stopCallParent(event){
+
+  function closeModal(event) {
+    stopCallParent(event);
+    event.data.modalName.css('display', 'none');
+    //โดยปกติแล้วโปรแกรมจะมองว่าการคลิกปุ่มที่อยู่ใน child เป็นการคลิก parentด้วย ถึงทำให้ handler ของ parent ทำงานอีกครั้งหลังจาก handler ของ child จบลง
+    //การใช้ stopPropagation เพื่อไม่ให้การ click มีผลต่อ parent ด้วย
+
+    //Propagation หมายถึงการเผยแผ่
+    console.log('close by button-close : ' + event.data.modalName[0].className);
+
+  }
+
+  function closeModalOutside(event) {
+    stopCallParent(event);
+    event.data.modalName.css('display', 'none');
+    console.log('close by outside : ' + event.data.modalName[0].className);
+  }
+
+  function stopCallParent(event) {
     event.stopPropagation(); //เมื่อมีการคลิกใน dialog จะ ไม่ให้ไปกระทบกับส่วนอื่น
-    console.log('click in :',event.currentTarget);
+    console.log('click in :', event.currentTarget);
     // console.log('Modal dialog');
   }
-  function toggleShowLink(){
+
+  function toggleShowLink() {
     //set การกดปุ่ม hamburger ให้สลับการใช้งาน class Active เพื่อแสดง animation ของ .navbar-hamburger.active (ขีดไหลลง และ ลูกศรเปลี่ยนทิศตาม css)
-    $btnHamburger.toggleClass('active'); 
+    $btnHamburger.toggleClass('active');
 
     $link.toggleClass('active'); //สั่งให้ navbar ใช้คลาส active เพื่อสไลด์ลงมา
   }
-  
-    
+
+
 })();
 
-let Menu = (function(){
-})();
+let Menu = (function () {})();
 
-let Slideshow = (function(){ 
-    let index = 0;
-    let nowIndex = 0;
-    let imagesList = [];
-    
-    const time = 2000;
-    const pathImage ='assets/imgs/slideshow/';
-    const url = 'http://localhost:2000/slideShow';
-    
-    //Catch DOM
-    let $slideShowContainer = $('.container').find('.banner-slideshow');
-    let $slideShow = $slideShowContainer.find('img');
-    let $btnBack = $slideShowContainer.find('#slideShowBtnBack');
-    let $btnNext = $slideShowContainer.find('#slideShowBtnNext');
-    let $selectorContainer = $slideShowContainer.find('.slideshow-selector');
+let Slideshow = (function () {
+  let index = 0;
+  let nowIndex = 0;
+  let imagesList = [];
 
-    //Bind event
-    $btnBack.on('click',{slide:'back'},setImageSelector);
-    $btnNext.on('click',{slide:'next'},setImageSelector);
-    $selectorContainer.on('click','span',setImageSelector);
-   
-    //set default html
-      getImages(ResImgsList =>{
-        imagesList = ResImgsList;
-        setAmountSelector(imagesList.length);
-        setSlideLoop();
-      });
-     
+  const time = 2000;
+  const pathImage = 'assets/imgs/slideshow/';
+  const url = 'http://localhost:2000/slideShow';
 
-    function getImages(callback){
-      fetchHttp(url,'GET','',callback);
+  //Catch DOM
+  let $slideShowContainer = $('.container').find('.banner-slideshow');
+  let $slideShow = $slideShowContainer.find('img');
+  let $btnBack = $slideShowContainer.find('#slideShowBtnBack');
+  let $btnNext = $slideShowContainer.find('#slideShowBtnNext');
+  let $selectorContainer = $slideShowContainer.find('.slideshow-selector');
+
+  //Bind event
+  $btnBack.on('click', {
+    slide: 'back'
+  }, setImageSelector);
+  $btnNext.on('click', {
+    slide: 'next'
+  }, setImageSelector);
+  $selectorContainer.on('click', 'span', setImageSelector);
+
+  //set default html
+  getImages(ResImgsList => {
+    imagesList = ResImgsList;
+    setAmountSelector(imagesList.length);
+    setSlideLoop();
+  });
+
+
+  function getImages(callback) {
+    fetchHttp(url, 'GET', '', callback);
+  }
+
+
+
+  function setSlideLoop() {
+    $slideShow.attr('src', pathImage + imagesList[index]);
+    nowIndex = index; //backup Real index before index++ 
+    setPositionSelector(index);
+    if (index < imagesList.length - 1) {
+      index++;
+    } else {
+      index = 0;
     }
-   
+    setTimeout(setSlideLoop, time);
+  }
 
-
-    function setSlideLoop(){
-      $slideShow.attr('src',pathImage+imagesList[index]);
-      nowIndex = index; //backup Real index before index++ 
-      setPositionSelector(index);
-      if(index <imagesList.length-1){
-        index++;
-      }else{
-        index = 0;
-      }
-      setTimeout(setSlideLoop,time);
+  function setAmountSelector(count) {
+    for (let i = 0; i < count; i++) {
+      $selectorContainer.append(`<span>&bull;</span>`);
     }
+  }
 
-    function setAmountSelector(count){   
-      for(let i = 0; i<count; i++){
-        $selectorContainer.append(`<span>&bull;</span>`);
-      }
-    }
+  function setImageSelector(event) {
 
-    function setImageSelector(event){
-   
-      if(event.data){
-          if(event.data.slide == 'next'){   
-              if(index>nowIndex){
-                index = nowIndex;
-              }
-                index = index+1;                                     
-              if(index == imagesList.length){
-                  index = 0;
-              }
-          }else{
-              console.log(index, nowIndex);
-              if(index>nowIndex){
-                index = nowIndex;
-              }
-              index = index-1;       
-              if(isNaN(index) ||index < 0){
-                  index = imagesList.length-1; 
-              }
-          }
-        
-      }else{
-          let target = $selectorContainer.find('span').index($(event.target));
-          index = target;
-      }
-     
-      $slideShow.attr('src',pathImage+imagesList[index]);
-      setPositionSelector(index);
-      nowIndex = index;
-    }
-    function setPositionSelector(indexTarget){
-      for(let i = 0; i < imagesList.length; i++){
-        if(i == indexTarget){
-          $selectorContainer.children().eq(indexTarget).css('color','#fff');
-        }else{
-          $selectorContainer.children().eq(i).css('color','rgba(0, 0, 0, 0.6)');
+    if (event.data) {
+      if (event.data.slide == 'next') {
+        if (index > nowIndex) {
+          index = nowIndex;
+        }
+        index = index + 1;
+        if (index == imagesList.length) {
+          index = 0;
+        }
+      } else {
+        console.log(index, nowIndex);
+        if (index > nowIndex) {
+          index = nowIndex;
+        }
+        index = index - 1;
+        if (isNaN(index) || index < 0) {
+          index = imagesList.length - 1;
         }
       }
+
+    } else {
+      let target = $selectorContainer.find('span').index($(event.target));
+      index = target;
     }
-  
+
+    $slideShow.attr('src', pathImage + imagesList[index]);
+    setPositionSelector(index);
+    nowIndex = index;
+  }
+
+  function setPositionSelector(indexTarget) {
+    for (let i = 0; i < imagesList.length; i++) {
+      if (i == indexTarget) {
+        $selectorContainer.children().eq(indexTarget).css('color', '#fff');
+      } else {
+        $selectorContainer.children().eq(i).css('color', 'rgba(0, 0, 0, 0.6)');
+      }
+    }
+  }
+
 })();
 
-let Promotion = (function(){
-    
-    //Catch DOM
-    let $promotionContainer = $('.container').find('.banner .banner-container-grid .banner-menu .banner-slideshow_promotion');
-    let $slideShowImagesContainer = $promotionContainer.find('.slideshow_imgs_container');
-    let $btnBack = $promotionContainer.children('#btnBack');
-    let $btnNext = $promotionContainer.children('#btnNext');
+let Promotion = (function () {
 
-    let lengthDiv = $slideShowImagesContainer.find('>*').length;
-    let count = 0;
+  //Catch DOM
+  let $promotionContainer = $('.container').find('.banner .banner-container-grid .banner-slideshow_promotion');
+  let $slideShowImagesContainer = $promotionContainer.find('.slideshow_promotion_container');
+  let $slideItem = $slideShowImagesContainer.children('.promotion-item');
+  let $btnBack = $promotionContainer.children('#btnBack');
+  let $btnNext = $promotionContainer.children('#btnNext');
 
-    //Bind event
-    $btnBack.on('click',scrollBack);
-    $btnNext.on('click',scrollNext);
-    
+  let widthItem = $slideItem.width() + 5;
+  let count = 0;
+  let lengthOfItem = $slideShowImagesContainer.find('>*').length;
 
-    function scrollBack(){
+
+
+  //Bind event
+  $btnBack.on('click', scrollBack);
+  $btnNext.on('click', scrollNext);
+
+
+  function scrollBack() {
+    // $slideShowImagesContainer.scrollLeft(setScaleScroll(--count))
+    $slideShowImagesContainer.animate({
+      scrollLeft: setScaleScroll(--count)
+    });
+  }
+
+  function scrollNext() {
+    // $slideShowImagesContainer.scrollLeft(setScaleScroll(++count)); 
+    $slideShowImagesContainer.animate({
+      scrollLeft: setScaleScroll(++count)
+    });
+  }
+
+  function setScaleScroll(c) {
+    if (c < 0) {
+      c = 0;
+      count = 0;
+    } else if (c > lengthOfItem - 1) {
+      c = lengthOfItem - 1;
+      count = lengthOfItem - 1;
     }
-    function scrollNext(){
-      $slideShowImagesContainer.scrollLeft += 50;
-      console.log($slideShowImagesContainer.scrollLeft);
-      
-      
-      // $slideShowImagesContainer.animate({
-      //   scrollLeft: 20
-      // });
-    }
+    return widthItem * c;
+  }
 })();
 
-let Admind =(function(){
+let Admin = (function () {
   let $linkToAdminPage = $('.footer-grid').find('.footer-grid-child');
-  $linkToAdminPage.on('click',()=>{
+  $linkToAdminPage.on('click', () => {
     window.open('/admin');
   });
-    
+
 })();
 
 
 
 
-function fetchHttp(url,method,data ='',callbackFunc){ 
-   let xhr = new XMLHttpRequest();
-   let urlServ = url+data;
-   xhr.open(method,urlServ,true);
-   xhr.setRequestHeader('Content-type','text/plain');
-   xhr.onreadystatechange = resFromServ;
-   xhr.send();
+function fetchHttp(url, method, data = '', callbackFunc = () => {}) {
+  let xhr = new XMLHttpRequest();
+  let urlServ = url + data;
+  xhr.open(method, urlServ, true);
+  xhr.setRequestHeader('Content-type', 'text/plain');
+  xhr.onreadystatechange = resFromServ;
+  xhr.send();
 
-  function resFromServ(){
-      if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-        let dataRespond = JSON.parse(xhr.responseText);
-        callbackFunc(dataRespond);
-      }
+  function resFromServ() {
+    if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+      let dataRespond = JSON.parse(xhr.responseText);
+      callbackFunc(dataRespond);
+    }
   }
 }
 
